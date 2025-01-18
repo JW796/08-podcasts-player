4596636b0b78478cef45f314b52f0bd947dec918
James Joseph <98562132+JW796@users.noreply.github.com>
2025-01-18 11:54:40 -0500

    Logout bug fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import {jwtDecode} from "jwt-decode";
+import { UserContext } from "./contexts/UserContext";
+import { jwtDecode } from "jwt-decode";
 
 function App() {
   const [user, setUser] = useState({});
@@ -8,44 +9,46 @@ function App() {
     let user = jwtDecode(res.credential);
     setUser(user);
     setLoggedIn(true);
-  }
+  };
 
   const handleLogOut = () => {
     setLoggedIn(false);
     setUser({});
   };
 
-  useEffect (() => {
+  useEffect(() => {
     /* global google */
     google.accounts.id.initialize({
       client_id:
         "875796848013-7hfgpnjsnvuha9geds6t5m86nsmqmdm3.apps.googleusercontent.com",
       callback: handleCallBack,
     });
-    google.accounts.id.renderButton(
-      document.getElementById("SignIn"),
-      {theme: "outline",size: "large"}
-    )
-  }, [])
+    google.accounts.id.renderButton(document.getElementById("SignIn"), {
+      theme: "outline",
+      size: "large",
+    });
+  }, [loggedIn]);
   return (
-    <div className="flex flex-col items-center">
-      <h1 className="text-xl mt-5 py-2 text-center">08 - Podcasts Player</h1>
-      {loggedIn ? (
-        <>
-        <button
-        className="border py-1 px-3 rounded-full my-2"
-        onClick={handleLogOut}
-        >
-          Log Out
-        </button>
-        <h3>Hi, {user.name}</h3>
-        </>
-    ):(
-    <>
-      <div id="SignIn"></div>
-    </>
-    )}
-    </div>
+    <UserContext.provider value={[user, setUser]}>
+      <div className="flex flex-col items-center">
+        <h1 className="text-xl mt-5 py-2 text-center">08 - Podcasts Player</h1>
+        {loggedIn ? (
+          <>
+            <button
+              className="border py-1 px-3 rounded-full my-2"
+              onClick={handleLogOut}
+            >
+              Log Out
+            </button>
+            <h3>Hi, {user.given_name}</h3>
+          </>
+        ) : (
+          <>
+            <div id="SignIn"></div>
+          </>
+        )}
+      </div>
+    </UserContext.provider>
   );
 }
 
